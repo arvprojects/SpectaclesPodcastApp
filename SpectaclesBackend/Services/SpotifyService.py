ffbbcbfd9e6616ff76735c03f0aa9debd7130e45
arvprojects <arvprojects99@gmail.com>
2025-01-11 13:59:19 -0600

    fdas
    dsf

--- SpectaclesBackend/Services/SpotifyService.py
+++ SpectaclesBackend/Services/SpotifyService.py
@@ -145,9 +145,11 @@ class SpotifyService:
         newdata=data
         newdata["spotify_auth_code"] = access_token
         newdata["spotify_refresh_token"] = refresh_token
+        userid = data['id']
         newdata.pop('id',None)
+        
         current_app.logger.info("spotify access "+ access_token+" "+refresh_token)
-        url = f"{current_app.config['PLATFORM_BACKEND_URL']}/users/{data['id']}"
+        url = f"{current_app.config['PLATFORM_BACKEND_URL']}/users/{userid}"
         response = requests.put(url, json=newdata)
         
 
