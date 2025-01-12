c2817559a6d0cf7e748e5e263c2d2d9f1948f317
adityapawar50 <aditpawar50@gmail.com>
2025-01-12 12:41:31 -0600

    project frontend

--- podcastarwebapp/src/App.js
+++ podcastarwebapp/src/App.js
@@ -16,6 +16,8 @@ const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const Userhome = React.lazy(() => import('./views/pages/homepage/Userhome'))
+const RemoveUserPage = React.lazy(() => import('./views/pages/unregister/RemoveUserPage'))
 
 const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
@@ -46,7 +48,9 @@ const App = () => {
       >
         <Routes>
           <Route exact path="/login" name="Login Page" element={<Login />} />
+          <Route exact path="/home" name="userhome" element={<Userhome />} />
           <Route exact path="/register" name="Register Page" element={<Register />} />
+          <Route exact path="/unregister" name="Unregister Page" element={<RemoveUserPage />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
           <Route path="*" name="Home" element={<DefaultLayout />} />
--- podcastarwebapp/src/views/pages/login/Login.js
+++ podcastarwebapp/src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import {
   CButton,
   CCard,
@@ -17,13 +17,14 @@ import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 
 const Login = () => {
-  const [email, setEmail] = useState('')
+  const navigate = useNavigate()
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   const handleLogin = () => {
     console.log('Login clicked')
+    navigate('/home', { state: { username: username } })
   }
-
 
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
@@ -40,7 +41,12 @@ const Login = () => {
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
                       </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
+                      <CFormInput
+                        placeholder="Snapchat Username"
+                        autoComplete="Snapchat username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
@@ -50,37 +56,38 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                       />
                     </CInputGroup>
-                    <CRow>
+                    <CRow className="mb-4">
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
+                        <CButton color="primary" className="px-4" onClick={handleLogin}>
                           Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
                         </CButton>
                       </CCol>
                     </CRow>
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
+              <CCard className="text-white bg-primary py-5">
+                <CCardBody>
+                  <CForm>
+                    <h1 className="me-2">Not a User?</h1>
                     <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
+                      <CButton color="success" className="px-4">
+                        Register
                       </CButton>
                     </Link>
-                  </div>
+                    <h1>Delete Account?</h1>
+                    <CButton
+                      color="danger"
+                      className="px-4"
+                      onClick={() => navigate('/unregister')}
+                    >
+                      Delete Account
+                    </CButton>
+                  </CForm>
                 </CCardBody>
               </CCard>
             </CCardGroup>
